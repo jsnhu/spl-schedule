fccc4cf8727a29e355f735bec0d29fc6084a11e0
jsnhu <47902466+jsnhu@users.noreply.github.com>
2019-03-24 17:43:56 -0700

    formatting

--- standardschedule.jl
+++ standardschedule.jl
@@ -106,49 +106,49 @@ m = Model(solver = GLPKSolverMIP())
 @variable(m, x[1:staff, 1:shift], Bin)
 
 # maximize preference score sum
-@objective(m, Max, sum(pref_matrix[i,j]*x[i,j] for i in 1:staff, j in 1:shift))
+@objective(m, Max, sum(pref_matrix[i, j] * x[i, j] for i in 1:staff, j in 1:shift))
 
 # constraints
 
 # cons1: exactly one person per shift
 for j in 1:shift
-    @constraint(m, sum( x[i,j] for i in 1:staff) == 1)
+    @constraint(m, sum( x[i, j] for i in 1:staff) == 1)
 end
 
 # cons2: maximum 4 shifts per week per person
 for i in 1:staff
-    @constraint(m, sum( x[i,j] for j in 1:shift) <= 4)
+    @constraint(m, sum( x[i, j] for j in 1:shift) <= 4)
 end
 
 # cons3: minimum 1 shift per week per person
 for i in 1:staff
-    @constraint(m, sum( x[i,j] for j in 1:shift) >= 1)
+    @constraint(m, sum( x[i, j] for j in 1:shift) >= 1)
 end
 
 # cons4: no employee works both Sat and Sun in one weekend
 for i in 1:staff
-    @constraint(m, sum( x[i,j] for j in union(sat_shift,sun_shift)) <= 1)
+    @constraint(m, sum( x[i ,j] for j in union(sat_shift, sun_shift)) <= 1)
 end
 
 # cons5: desk employees cannot work shelving shifts
 for i in desk_staff
-    @constraint(m, sum( x[i,j] for j in shel_shift) == 0)
+    @constraint(m, sum( x[i, j] for j in shel_shift) == 0)
 end
 
 # cons6: shelving employees cannot work desk shifts
 for i in shel_staff
-    @constraint(m, sum( x[i,j] for j in desk_shift) == 0)
+    @constraint(m, sum( x[i, j] for j in desk_shift) == 0)
 end
 
 # cons7: nobody works 2 shifts (or more) in one day:
 for i in 1:staff
-    @constraint(m,  sum( x[i,j] for j in mon_shift) <= 1)
-    @constraint(m,  sum( x[i,j] for j in tue_shift) <= 1)
-    @constraint(m,  sum( x[i,j] for j in wed_shift) <= 1)
-    @constraint(m,  sum( x[i,j] for j in thu_shift) <= 1)
-    @constraint(m,  sum( x[i,j] for j in fri_shift) <= 1)
-    @constraint(m,  sum( x[i,j] for j in sat_shift) <= 1)
-    @constraint(m,  sum( x[i,j] for j in sun_shift) <= 1)
+    @constraint(m, sum( x[i, j] for j in mon_shift) <= 1)
+    @constraint(m, sum( x[i, j] for j in tue_shift) <= 1)
+    @constraint(m, sum( x[i, j] for j in wed_shift) <= 1)
+    @constraint(m, sum( x[i, j] for j in thu_shift) <= 1)
+    @constraint(m, sum( x[i, j] for j in fri_shift) <= 1)
+    @constraint(m, sum( x[i, j] for j in sat_shift) <= 1)
+    @constraint(m, sum( x[i, j] for j in sun_shift) <= 1)
 end
 
 # print(m)
